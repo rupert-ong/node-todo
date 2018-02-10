6aaa75f332a89e5f7255f139b06f51b3c68f3d58
rupert-ong <rupert.ong@gmail.com>
2018-02-10 13:26:58 -0500

    Create basic HTTP server

--- app.js
+++ app.js
@@ -1,30 +1,15 @@
-const fs = require('fs');
+const http = require('http');
 
-// Read and Write Files
-/* fs.readFile(__dirname + '/README.md', 'utf8', (err, data) => {
-  fs.writeFile(__dirname + '/WRITEME.md', data, (err) => { if(err) throw err; });
-  
-  // Delete Files
-  fs.unlink(__dirname + '/WRITEME.md', (err) => {
-    if(err) console.log(`Files doesn't exist`);
+const server = http.createServer((req, res) => {
+  res.writeHead(200, {
+    'Content-type': 'text/plain'
   });
-}); */
+  res.end('Hello world');
+});
 
-// Delete directory (Note we are blocking with Sync)
-if(fs.existsSync('stuff')){
-  // Can only remove a directory if it's empty
-  for(file of fs.readdirSync('stuff')){
-    fs.unlinkSync(`./stuff/${file}`);
-  };
-  fs.rmdirSync('stuff');
-}
+const host = process.env.HOST || '127.0.0.1';
+const port = process.env.PORT || 3000;
 
-// Create Directories
-fs.mkdir('stuff', (err)=> {
-  if(err) console.log('File already exists');
-  fs.readFile('./README.md', 'utf8', (err, data)=> {
-    if(err) return;
-    console.log('readFile async call: ' + data);
-    fs.writeFile('./stuff/WRITEME.md', data, (err) => { if(err) return; })
-  })
+server.listen(3000, '127.0.0.1', () => {
+  console.log(`Listening on ${host}:${port}`);
 });
