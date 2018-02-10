241cdca4aa8045fc921d87080adccfdb0948bcae
rupert-ong <rupert.ong@gmail.com>
2018-02-10 15:04:00 -0500

    Fix server typo

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
+  console.log(`Request was made ${req.url}`);
   res.writeHead(200, {
     'Content-type': 'text/plain'
   });
@@ -10,6 +11,7 @@ const server = http.createServer((req, res) => {
 const host = process.env.HOST || '127.0.0.1';
 const port = process.env.PORT || 3000;
 
-server.listen(3000, '127.0.0.1', () => {
+// set PORT=5000 && set HOST=127.0.0.1 && node app
+server.listen(port, host, () => {
   console.log(`Listening on ${host}:${port}`);
 });
