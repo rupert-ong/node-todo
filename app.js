c50064b79ab6e2c9fcf434347e780e869b9e4077
rupert-ong <rupert.ong@gmail.com>
2018-02-10 16:05:27 -0500

    Add basic routing

--- app.js
+++ app.js
@@ -3,13 +3,24 @@ const fs = require('fs');
 
 // Server
 const server = http.createServer((req, res) => {
-  const readStream = fs.createReadStream(__dirname + '/index.html', 'utf8');
-
-  // Pipe readable stream into response (a writable stream)
-  readStream.pipe(res);
-
-  console.log(`Request was made ${req.url}`);
-  res.writeHead(200, { 'Content-type': 'text/html' });
+  if (req.url === '/home' || req.url === '/') {
+    res.writeHead(200, { 'Content-type': 'text/html' });
+    fs.createReadStream(__dirname + '/index.html').pipe(res);
+  } else if (req.url === '/contact') {
+    res.writeHead(200, { 'Content-type': 'text/html' });
+    fs.createReadStream(__dirname + '/contact.html').pipe(res);
+  } else if (req.url === '/api/authors') {
+    res.writeHead(200, { 'Content-type': 'application/json' });
+    var obj = {
+      name: 'Roman Mars',
+      job: 'Podcaster',
+      age: 30
+    }
+    res.end(JSON.stringify(obj));
+  } else {
+    res.writeHead(404, { 'Content-type': 'text/html' });
+    fs.createReadStream(__dirname + '/404.html').pipe(res);
+  }
 });
 
 const host = process.env.HOST || '127.0.0.1';
