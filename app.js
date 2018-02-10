0d57bf4e9fbc7cf28f815ec5219adb5b86dd6575
rupert-ong <rupert.ong@gmail.com>
2018-02-10 15:39:58 -0500

    Use pipe to write into HTTP server response

--- app.js
+++ app.js
@@ -1,20 +1,15 @@
 const http = require('http');
 const fs = require('fs');
 
-// Streams
-const readStream = fs.createReadStream(__dirname + '/readme.txt', 'utf8');
-const writeStream = fs.createWriteStream(__dirname + '/writeme.txt', 'utf8');
-
-// Shorthand to read then write
-readStream.pipe(writeStream);
-
 // Server
-/* const server = http.createServer((req, res) => {
+const server = http.createServer((req, res) => {
+  const readStream = fs.createReadStream(__dirname + '/readme.txt', 'utf8');
+
+  // Pipe readable stream into response (a writable stream)
+  readStream.pipe(res);
+
   console.log(`Request was made ${req.url}`);
-  res.writeHead(200, {
-    'Content-type': 'text/plain'
-  });
-  res.end('Hello world');
+  res.writeHead(200, { 'Content-type': 'text/plain' });
 });
 
 const host = process.env.HOST || '127.0.0.1';
@@ -23,4 +18,4 @@ const port = process.env.PORT || 3000;
 // set PORT=5000 && set HOST=127.0.0.1 && node app
 server.listen(port, host, () => {
   console.log(`Listening on ${host}:${port}`);
-}); */
+});
