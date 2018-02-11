624e43fe6aec0d353ae468f0d72db481dc6af2f4
rupert-ong <rupert.ong@gmail.com>
2018-02-10 20:49:52 -0500

    Use static middleware for styles

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const app = express();
 
 app.set('view engine', 'ejs');
 
+app.use('/assets', express.static('public/assets/'));
+
 app.get('/', (req, res, next) => {
   res.render('index', { title:'Homepage' });
 });
