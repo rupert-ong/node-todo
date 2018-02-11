79dd2dc26c961c19c3b1cf744064fb62f317db8d
rupert-ong <rupert.ong@gmail.com>
2018-02-10 19:37:29 -0500

    Add express and nodemon dependencies. Add basic routes.

--- app.js
+++ app.js
@@ -1,32 +1,19 @@
-const http = require('http');
-const fs = require('fs');
+const express = require('express');
+const app = express();
 
-// Server
-const server = http.createServer((req, res) => {
-  if (req.url === '/home' || req.url === '/') {
-    res.writeHead(200, { 'Content-type': 'text/html' });
-    fs.createReadStream(__dirname + '/index.html').pipe(res);
-  } else if (req.url === '/contact') {
-    res.writeHead(200, { 'Content-type': 'text/html' });
-    fs.createReadStream(__dirname + '/contact.html').pipe(res);
-  } else if (req.url === '/api/authors') {
-    res.writeHead(200, { 'Content-type': 'application/json' });
-    var obj = {
-      name: 'Roman Mars',
-      job: 'Podcaster',
-      age: 30
-    }
-    res.end(JSON.stringify(obj));
-  } else {
-    res.writeHead(404, { 'Content-type': 'text/html' });
-    fs.createReadStream(__dirname + '/404.html').pipe(res);
-  }
+app.get('/', (req, res, next) => {
+  res.status(200);
+  res.sendFile(__dirname + '/index.html');
 });
 
-const host = process.env.HOST || '127.0.0.1';
-const port = process.env.PORT || 3000;
-
-// set PORT=5000 && set HOST=127.0.0.1 && node app
-server.listen(port, host, () => {
-  console.log(`Listening on ${host}:${port}`);
+app.get('/contact', (req, res, next) => {
+  res.status(200);
+  res.sendFile(__dirname + '/contact.html');
 });
+
+app.listen(3000, () => {
+  console.log(`Express app is listening on port 3000`);
+})
+
+
+module.exports = app;
