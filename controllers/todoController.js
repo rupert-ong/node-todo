55fa1c5397116b7bbd56f633b7fed6b8a36849ef
rupert-ong <rupert.ong@gmail.com>
2018-02-12 15:15:41 -0500

    Add dummy data and get, post, delete routes

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -1,11 +1,25 @@
+const bodyParser = require('body-parser');
+const urlEncoder = bodyParser.urlencoded({ extended: false });
+
+let data = [
+  {item: 'Get milk'},
+  {item: 'Kick ass'},
+  {item: 'Chew bubblegum'}
+];
+
 module.exports = function(app) {
   app.get('/todo', (req, res) => {
-    res.render('todo');
+    res.render('todo', {
+      todos: data
+    });
   });
-  app.post('/todo', (req, res) => {
-
+  app.post('/todo', urlEncoder, (req, res) => {
+    console.log(req.body);
+    data.push(req.body);
+    res.json(data);
   });
-  app.delete('/todo', (req, res) => {
-
+  app.delete('/todo/:item', (req, res) => {
+    data = data.filter((todo) => todo.item.replace(/ /g, '-') !== req.params.item);
+    res.json(data);
   });
 }
