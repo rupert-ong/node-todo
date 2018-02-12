716de06fd87640c0f14804ab19feb54465edf314
rupert-ong <rupert.ong@gmail.com>
2018-02-12 14:48:08 -0500

    Setup todoController

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
 const express = require('express');
 const app = express();
 
+// controllers
+const todoController = require('./controllers/todoController');
+
 // set up template engine
 app.set('view engine', 'ejs');
 
 // static files routing
 app.use(express.static('./public'));
+
+// fire controllers
+todoController(app);
 
 // listen to port
 app.listen(3000, () => {
---
+++ controllers/todoController.js
@@ -0,0 +1,11 @@
+module.exports = function(app) {
+  app.get('/todo', (req, res) => {
+
+  });
+  app.post('/todo', (req, res) => {
+
+  });
+  app.delete('/todo', (req, res) => {
+
+  });
+}
