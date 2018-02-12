5bb439f0dffb6bcabb04ecc7782e2064a70a5056
rupert-ong <rupert.ong@gmail.com>
2018-02-12 09:18:40 -0500

    Remove unnecessary files and reset app.js

--- app.js
+++ app.js
@@ -1,37 +1,13 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 const app = express();
 
+// set up template engine
 app.set('view engine', 'ejs');
 
-app.use('/assets', express.static('public/assets/'));
+// static files routing
+app.use(express.static('./public'));
 
-app.use(bodyParser.urlencoded({ extended: false }));
-
-app.get('/', (req, res, next) => {
-  res.render('index', { title:'Homepage' });
-});
-
-app.get('/contact', (req, res, next) => {
-  res.render('contact', { title: 'Contact Us', qs: req.query } );
-});
-
-app.post('/contact', (req, res, next) => {
-  console.log(req.body);
-  res.render('contact-success', { title: 'Contact Us', data: req.body } );
-});
-
-app.get('/profile/:name', (req, res, next) => {
-  const data = {age:29, job: 'ninja', hobbies: ['eating', 'fighting', 'fishing']};
-  res.render('profile', {
-    title: `Profile of ${req.params.name}`,
-    person: req.params.name,
-    data: data
-  });
-});
-
+// listen to port
 app.listen(3000, () => {
   console.log(`Express app is listening on port 3000`);
 });
-
-module.exports = app;
