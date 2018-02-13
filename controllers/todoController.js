9da4357f58f90af31ddd61b89acc49a3c4b8d2f8
rupert-ong <rupert.ong@gmail.com>
2018-02-12 20:26:39 -0500

    Add Todo model and schema. Update controller routes.

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -1,25 +1,41 @@
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const Todo = require('../models/todo');
+
 const urlEncoder = bodyParser.urlencoded({ extended: false });
 
-let data = [
-  {item: 'Get milk'},
-  {item: 'Kick ass'},
-  {item: 'Chew bubblegum'}
-];
+mongoose.connect(process.env.MONGODB_CONNECTION);
+mongoose.Promise = global.Promise;
 
-module.exports = function(app) {
+module.exports = function (app) {
   app.get('/todo', (req, res) => {
-    res.render('todo', {
-      todos: data
-    });
+    Todo.find()
+      .exec()
+      .then(todos => {
+        res.status(200).render('todo', { todos: todos });
+      })
+      .catch(err => {
+        res.status(200).render('todo', { todos: null });
+      });
   });
   app.post('/todo', urlEncoder, (req, res) => {
-    console.log(req.body);
-    data.push(req.body);
-    res.json(data);
+    Todo.create(req.body)
+      .then(result => {
+        console.log(`Created: ${result}`);
+        res.status(201).json(result);
+      })
+      .catch(err => {
+        res.status(500).json({ error: err.mesage });
+      });
   });
-  app.delete('/todo/:item', (req, res) => {
-    data = data.filter((todo) => todo.item.replace(/ /g, '-') !== req.params.item);
-    res.json(data);
+  app.delete('/todo', urlEncoder, (req, res) => {
+    Todo.findOneAndRemove({ _id: req.body.id })
+      .exec()
+      .then(result => {
+        console.log(`Deleted: ${result}`);
+        res.status(200).json(result);
+      })
+      .catch(err => { res.status(500).json({ error: err.message }) });
   });
 }
