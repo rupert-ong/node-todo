1bab1933c4a1ec3cf3ae9c8a4ab6f4f11780028b
rupert-ong <rupert.ong@gmail.com>
2018-02-14 10:15:15 -0500

    Refactor front-end JS

--- public/assets/todo-list.js
+++ public/assets/todo-list.js
@@ -9,28 +9,33 @@ function initApplication() {
   form.onsubmit = (e) => {
     const item = form.getElementsByTagName('input')[0];
     const todo = { item: item.value };
-    let xhr = new XMLHttpRequest();
-    xhr.open('POST', '/todo', true);
-    xhr.setRequestHeader('Content-type', 'application/json');
-    xhr.onload = () => {
-      if (xhr.status >= 200 && xhr.status < 400) { location.reload(); }
-    }
-    xhr.onerror = (e) => { alert('Connection Error: ' + JSON.parse(req.responseText).error); }
-    xhr.send(JSON.stringify(todo));
+    const callback = () => { location.reload(); }
+    const postXhr = createXHR({method: 'POST', url: '/todo', contentType: 'application/json', data: todo, callback: callback});
     e.preventDefault();
   }
 
   document.addEventListener('click', (e) => {
-    if (e.target.tagName == 'LI') {
-      const data = { id: e.target.dataset.id };
-      let xhr = new XMLHttpRequest();
-      xhr.open('DELETE', '/todo', true);
-      xhr.setRequestHeader('Content-type', 'application/json');
-      xhr.onload = () => {
-        if (xhr.status >= 200 && xhr.status < 400) { location.reload(); }
-      }
-      xhr.onerror = (e) => { alert('Connection Error: ' + JSON.parse(req.responseText).error); }
-      xhr.send(JSON.stringify(data));
+    const elem = e.target;
+    if (elem.tagName == 'LI' && elem.dataset.id) {
+      const data = { id: elem.dataset.id };
+      const callback = () => { location.reload(); }
+      const deleteXhr = createXHR({method: 'DELETE', url: '/todo', contentType: 'application/json', data: data, callback: callback});
     }
   }, false);
+}
+
+function createXHR(config) {
+  let xhr = new XMLHttpRequest();
+  let data = "";
+  if(config.data){
+    data = config.contentType === 'application/json' ? JSON.stringify(config.data) : config.data;
+  }
+  xhr.open(config.method, config.url, true);
+  xhr.setRequestHeader('Content-type', config.contentType);
+  xhr.onload = () => {
+    if (xhr.status >= 200 && xhr.status < 400) { if(config.callback && typeof config.callback === 'function') config.callback(); }
+  }
+  xhr.onerror = (e) => { alert('Connection Error: ' + JSON.parse(req.responseText).error); }
+  xhr.send(data);
+  return xhr;
 }
